81512a9d58ce3241b6926d4ca8cfacdb76be2a51
Alex-Hendy <alec.hendy@googlemail.com>
2026-05-22 10:26:11 +0100

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,20 @@
+document.getElementById('site-footer').innerHTML = `
+<footer>
+  <div class="footer-inner">
+    <div class="footer-main">
+      <strong>Glasgow YIMBY</strong> — a grassroots campaign for more homes in Glasgow
+    </div>
+    <div class="footer-links">
+      <a href="/get-involved.html">Get involved</a>
+      <a href="mailto:hello@glasgowyimby.com">hello@glasgowyimby.com</a>
+    </div>
+    <div class="footer-links footer-links-secondary">
+      <a href="/about/who-we-are.html">Who we are</a>
+      <a href="/about/glossary.html">Glossary</a>
+      <a href="/about/donate.html">Donate</a>
+      <a href="/about/funding.html">Our funding</a>
+      <a href="/about/privacy.html">Privacy policy</a>
+    </div>
+  </div>
+</footer>
+`;
